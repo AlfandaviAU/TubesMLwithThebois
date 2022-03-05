c6896787e5aba3f645b606858af6409eef108763
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2022-03-05 19:55:27 +0700

    push

--- Bagian A/main.py
+++ Bagian A/main.py
@@ -16,5 +16,14 @@ for layer in data:
         np.array(layer["data"]),
         np.array(layer["bias"])
     ))
+    data_n += 1
 
-print(data_layer[0])
+# print(data_layer)
+# print(data_layer[0])
+# print(data_n)
+
+target = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+for i in range (data_n):
+    # untuk data pertama
+    if (i == 0):
+        
