babfebcaed8a69b4015f81f4af62dc5f4dba9fb8
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2022-03-05 19:49:46 +0700

    p

--- Bagian A/main.py
+++ Bagian A/main.py
@@ -4,4 +4,17 @@ import json
 
 filename = "data.json"
 data = json.load(open(filename))
-print(data)
+
+data_layer = []
+data_n = 0
+
+for layer in data:
+    # iterasi setiap layer pada json obj
+    data_layer.append((
+        int(layer["n"]),
+        layer["fungsiAktivasi"],
+        np.array(layer["data"]),
+        np.array(layer["bias"])
+    ))
+
+print(data_layer[0])
