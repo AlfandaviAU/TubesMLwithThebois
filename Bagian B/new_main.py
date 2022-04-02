90683079f13ec92ff59105a518e9c079008348ed
makhlukcupu <rajuhmanusia@gmail.com>
2022-04-02 23:20:39 +0700

    dead

--- Bagian B/new_main.py
+++ Bagian B/new_main.py
@@ -34,14 +34,43 @@ def shuffle(x, y):
     p = np.random.permutation(len(x))
     return x[p], y[p]
 
+# # XOR
+# x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
+# y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
 # XOR
-x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
-y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+from keras.utils import np_utils
+from sklearn.datasets import load_iris
+iris = load_iris()
+x = iris.data
+y = iris.target
 
-x_train_shuffled, y_train_shuffled = shuffle(x_train, y_train)
+# x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
+# y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+x_train, y_train = shuffle(x, y)
+x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1])
+#print(x_train)
 
 # ANN
-network = ANN(loadJSONData('xor_sigmoid.json'))
-network.fit(x_train_shuffled, y_train_shuffled, learning_rate=0.01, batch_size=3, max_iter=120, error_threshold=0.000001)
-out = network.predict(x_train)
-print(out)
+network = ANN(loadJSONData('xor_relu_copy.json'))
+print(network.predict(x_train[:5]))
+# print(x_train_shuffled)
+y_train = np_utils.to_categorical(y_train)
+y_train = y_train.reshape(y_train.shape[0], 1, y_train.shape[1])
+# print(y_train_shuffled)
+network.fit(x_train, y_train, learning_rate=0.01, batch_size=3, max_iter=100, error_threshold=0.0001)
+weights = []
+bias = []
+af = []
+for i in range(len(network.layers)):
+    weights.append(network.layers[i].weights.tolist())
+    bias.append(network.layers[i].bias.tolist())
+    af.append(network.layers[i].activationFunction)
+
+data = {"weights": weights, "bias":bias, "activation_function": af}
+with open('weights.json', 'w') as f:
+    json.dump(data, f)
+
+out = network.predict(x_train[:5])
+print("hasil = ", out)
+print("seharusnya = ", y_train[:5])
